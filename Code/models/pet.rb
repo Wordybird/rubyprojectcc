a55ba752af22ca4eb8a3705836cd31dfeeeaa2f4
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-25 11:18:16 +0000

    Created and tested function to confirm whether or not a pet can be adopted.

--- Code/models/pet.rb
+++ Code/models/pet.rb
@@ -9,6 +9,7 @@ class Pet
     @breed=options['breed']
     @days_in_captivity=options['days_in_captivity'].to_i
     @owner_id= nil || options['owner_id'].to_i
+    @is_adoptable = options['is_adoptable']
     @id=nil || options['id'].to_i
   end
 
@@ -35,6 +36,19 @@ class Pet
   def self.delete_all()
     sql="DELETE FROM pets"
     SqlRunner.run(sql)
+  end
+
+  def is_adoptable()
+    sql= "SELECT * FROM pets"
+    pets= SqlRunner.run(sql)
+    for pet in pets
+     if @days_in_captivity >30
+       pet = "This pet is adoptable."
+     else
+        pet = "This pet is not adoptable as it has not been with us for enough time."
+     end
+     return pet
+    end
   end
 
 end
