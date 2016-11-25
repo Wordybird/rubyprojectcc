32b36176109ae680e73194877a163fccc457528c
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-25 11:31:32 +0000

    Created index page for pets, which displays their details.

--- code/controller.rb
+++ code/controller.rb
@@ -2,3 +2,9 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/pet.rb')
 require_relative('models/owner.rb')
+
+get '/' do
+  @pets=Pet.all()
+  @owners=Owner.all()
+  erb(:index)
+end
