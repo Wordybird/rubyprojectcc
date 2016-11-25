3047b9ed377617dd89c957bc7ed9ec0538715afc
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-25 10:33:09 +0000

    Tested self.all, self.find and self.delete_all functions for owner file.

--- Code/db/seeds.rb
+++ Code/db/seeds.rb
@@ -1,10 +1,15 @@
-require_relative('../models/pet')
+#require_relative('../models/pet')
 require_relative('../models/owner')
 
 require ('pry-byebug')
 
 #Pet.delete_all()
-#Owner.delete_all()
+Owner.delete_all()
+
+owner1=Owner.new({"name"=>"John Smith"})
+owner1.save()
+
+owners=Owner.all()
 
 
 
---
+++ Code/models/owner.rb
@@ -0,0 +1,37 @@
+require_relative('../db/sql_runner')
+
+class Owner
+
+  attr_reader(:name, :url, :id)
+
+  def initialize(options)
+    @name=options['name']
+    @id=nil || options['id'].to_i
+  end
+
+  def save()
+    sql="INSERT INTO owners (name) VALUES ('#{name}') RETURNING *"
+    names=SqlRunner.run(sql)
+    @id = names.first()['id'].to_i
+  end
+
+  def self.all()
+    sql="SELECT * FROM owners"
+    owners = SqlRunner.run(sql)
+    result = owners.map { |owner| Owner.new(owner) }
+    return result
+  end
+
+  def self.find(id)
+    sql="SELECT * FROM owners WHERE id = #{id}"
+    owner= SqlRunner.run(sql)
+    result=Owner.new(owner.first)
+    return result
+  end
+
+  def self.delete_all()
+    sql="DELETE FROM owners"
+    SqlRunner.run(sql)
+  end
+
+end
