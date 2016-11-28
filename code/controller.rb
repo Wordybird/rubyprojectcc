9e62e8c90861f4d0d0fa4e95ca4b013ed13d9f96
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-28 20:22:00 +0000

    MINIMUM VIABLE PRODUCT ACHIEVED.

--- code/controller.rb
+++ code/controller.rb
@@ -4,7 +4,11 @@ require_relative('models/pet.rb')
 require_relative('models/owner.rb')
 
 get '/' do
+  return "Welcome to the animal shelter! Add /pets in your url bar to see the animals in our care."
+end
+
+get '/pets' do
   @pets=Pet.all()
   @owners=Owner.all()
-  erb(:index)
+  erb(:show)
 end
