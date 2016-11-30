2e89004d626954769551e6d4c1149499c80b90e6
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-30 09:47:49 +0000

    Worked around git to ensure that owner and pet ruby files will be committed correctly.

--- Code/models/owner.rb
+++ Code/models/owner.rb
@@ -5,32 +5,32 @@ class Owner
   attr_reader(:name, :id)
 
   def initialize(options)
-    @name=options['name']
-    @id=nil || options['id'].to_i
+    @name = options['name']
+    @id = nil || options['id'].to_i
   end
 
   def save()
-    sql="INSERT INTO owners (name) VALUES ('#{@name}') RETURNING *"
-    names=SqlRunner.run(sql)
+    sql = "INSERT INTO owners (name) VALUES ('#{@name}') RETURNING *"
+    names = SqlRunner.run(sql)
     @id = names.first()['id'].to_i
   end
 
   def self.all()
-    sql="SELECT * FROM owners"
+    sql = "SELECT * FROM owners"
     owners = SqlRunner.run(sql)
     result = owners.map { |owner| Owner.new(owner) }
     return result
   end
 
   def self.find(id)
-    sql="SELECT * FROM owners WHERE id = #{id}"
-    owner= SqlRunner.run(sql)
-    result=Owner.new(owner.first)
+    sql = "SELECT * FROM owners WHERE id = #{id}"
+    owner = SqlRunner.run(sql)
+    result = Owner.new(owner.first)
     return result
   end
 
   def self.delete_all()
-    sql="DELETE FROM owners"
+    sql = "DELETE FROM owners"
     SqlRunner.run(sql)
   end
 
